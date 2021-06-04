491308aa1862ab5b3fd2cda4011953bba4431d3e
John Kelleher <84353883+JKelleher00@users.noreply.github.com>
2021-06-04 15:30:34 -0400

    Initial commit

---
+++ src/main/java/achoo/Problem4.java
@@ -0,0 +1,15 @@
+package achoo;
+import java.util.*;
+
+public class Problem4 {
+    public static void main(String args[]){
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter a noun: ");
+        String one = a.nextLine();
+        System.out.print("Enter a verb: ");
+        String two = a.nextLine();
+        System.out.print("Enter a adjective: ");
+        String three = a.nextLine();
+        System.out.println("Is your " + three + " " + one + " running? Better go and " + two);
+    }
+}
